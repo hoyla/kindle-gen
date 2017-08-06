974c89fbab686d9f29f4f1760397297cee0a45c5
Alice Dee <alice.dee@guardian.co.uk>
2017-08-06 10:37:43 +0100

    Edits fields in article class to match Fingerpost
    Adds conversion between joda DateTime types with capiDateTime types
    - The apiclient dates are in capiDateTime format
    - New private formatter methods added for conversion between these types

--- src/main/scala/com/gu/kindlegen/Article.scala
+++ src/main/scala/com/gu/kindlegen/Article.scala
@@ -1,49 +1,57 @@
 package com.gu.kindlegen
 
 import com.github.nscala_time.time.Imports._
+import com.gu.contentapi.client.model.v1._
+import org.joda.time.format.ISODateTimeFormat
 
 case class Article(
     title: String,
-    status: String,
-    idString: String,
-    urgency: Int, // TODO: Use an enum
-    issueDate: DateTime,
-    releaseDate: DateTime,
-    pubDate: DateTime,
+    //    status: String, TODO: is status required?
+    //    idString: String, TODO: how is this generated and used?
+    //    urgency: Int, // TODO: Use an enum if required - fingerpost uses "2"
+    issueDate: CapiDateTime, // appears to be pub date in fingerpost, which is
+    // newspaperEditionDate in ContentFields
+    releaseDate: CapiDateTime, // appears to be pub date reformatted? see ln409 of
+    // fingerpost goa_kindle.pl file
+    pubDate: CapiDateTime, // appears to be pub date
     byline: String,
-    articleAbstract: String,
+    articleAbstract: String, // standfirst is used
     content: String
 ) {
   def toNitf: String = {
-    val formattedIssueDate = formatter.print(issueDate)
-    val formattedReleaseDate = formatter.print(releaseDate)
-    val formattedPubDate = formatter.print(pubDate)
+    val formattedIssueDate = formatter.print(isoFormatter.parseDateTime(capiIsoDateTimeToString(issueDate)))
+    val formattedReleaseDate = formatter.print(isoFormatter.parseDateTime(capiIsoDateTimeToString(releaseDate)))
+    val formattedPubDate = formatter.print(isoFormatter.parseDateTime(capiIsoDateTimeToString(pubDate)))
     s"""
-    |<?xml version="1.0" encoding="UTF-8"?>
-    |<nitf version="-//IPTC//DTD NITF 3.3//EN">
-    |<head>
-    |<title>$title</title>
-    |<docdata management-status="$status">
-    |<doc-id id-string="$idString" />
-    |<urgency ed-urg="$urgency" />
-    |<date.issue norm="$formattedIssueDate" />
-    |<date.release norm="$formattedReleaseDate" />
-    |<doc.copyright holder="guardian.co.uk" />
-    |</docdata>
-    |<pubdata type="print" date.publication="$formattedPubDate" />  
-    |</head>
-    |<body>
-    |<body.head>
-    |<hedline><hl1>$title</hl1></hedline>
-    |<byline>$byline</byline>
-    |<abstract>$articleAbstract</abstract>
-    |</body.head>
-    |<body.content>$content</body.content>
-    |<body.end />
-    |</body>
-    |</nitf>""".stripMargin
+       |<?xml version="1.0" encoding="UTF-8"?>
+       |<nitf version="-//IPTC//DTD NITF 3.3//EN">
+       |<head>
+       |<title>$title</title>
+       |<docdata management-status="usable">
+       |<doc-id id-string="999_title" />
+       |<urgency ed-urg="2" />
+       |<date.issue norm="$formattedIssueDate" />
+       |<date.release norm="$formattedReleaseDate" />
+       |<doc.copyright holder="guardian.co.uk" />
+       |</docdata>
+       |<pubdata type="print" date.publication="$formattedPubDate" />
+       |</head>
+       |<body>
+       |<body.head>
+       |<hedline><hl1>$title</hl1></hedline>
+       |<byline>$byline</byline>
+       |<abstract>$articleAbstract</abstract>
+       |</body.head>
+       |<body.content>$content</body.content>
+       |<body.end />
+       |</body>
+       |</nitf>""".stripMargin
   }
 
+  // formatter for use with parseDateTime to convert an isoDateTime: String to isoDateTime: DateTime
+  private def isoFormatter = ISODateTimeFormat.dateTime()
+  // formatter for use with parseDateTime or print to convert a DateTime: Long to DateTime: DateTime
   private def formatter = DateTimeFormat.forPattern("yyyyMMdd")
+  private def capiIsoDateTimeToString(dt: CapiDateTime): String = dt.iso8601
 
 }
