76861c1834a6a66a1f478f95f8a07a6ff71fa64d
Alice Dee <alice.dee@guardian.co.uk>
2017-08-07 15:35:11 +0100

    Adds Querier to lambda. Queries PrintSent API and creates Article object

--- src/main/scala/com/gu/kindlegen/Lambda.scala
+++ src/main/scala/com/gu/kindlegen/Lambda.scala
@@ -1,6 +1,19 @@
 package com.gu.kindlegen
 
+import java.io.File
+
 import com.amazonaws.services.lambda.runtime.Context
+import com.gu.contentapi.client._
+import com.gu.contentapi.client.model._
+import com.gu.contentapi.client.model.v1._
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.{ Await, Future }
+import scala.concurrent.duration._
+import scala.util.{ Failure, Success, Try }
+import Article._
+
+import scala.io.Source
 
 /**
  * This is compatible with aws' lambda JSON to POJO conversion.
@@ -48,3 +61,72 @@ object TestIt {
     println(Lambda.process(args.headOption.getOrElse("Alex"), Env()))
   }
 }
+
+object Querier {
+
+  class PrintSentContentClient(override val apiKey: String) extends GuardianContentClient(apiKey) {
+    // here I override the val for targetUrl from "http://content.guardianapis.com"
+    // the `preview.` is required in order to access the print-sent api.
+    override val targetUrl = "https://preview.content.guardianapis.com/content/print-sent"
+    // TODO: Find out if I will need capi Auth or the trailing "&user-tier=internal" that is used in the kindle-previewer
+
+  }
+  def readApiKey = {
+    val localUserHome = scala.util.Properties.userHome
+    val configSource = Source.fromFile(s"$localUserHome/.gu/kindle-gen.conf")
+    val key = configSource.getLines.mkString
+    configSource.close
+    key
+  }
+
+  def printSentResponse = {
+    val capiKey = readApiKey //TODO: keys
+    val capiClient = new PrintSentContentClient(capiKey)
+    val pageNum = 1
+    val query = SearchQuery()
+      .pageSize(1)
+      .showFields("all")
+    val response = Await.result(capiClient.getResponse(query), 5.seconds)
+    def title = response.results.map { content =>
+      content.fields.flatMap(x => x.headline).getOrElse("")
+    }
+    val body = response.results.map { content =>
+      content.fields.flatMap(x => x.body).getOrElse("")
+    }
+    val dateTime = response.results.map { content =>
+      content.fields.flatMap(x => x.newspaperEditionDate).get
+    }
+    val contentFields = response.results.map { content =>
+      content.fields
+    }
+    println(dateTime)
+    contentFields
+  }
+
+  def resultToArticle(response: Seq[Option[com.gu.contentapi.client.model.v1.ContentFields]]): Article = {
+    Article(
+      title = response.map { fields =>
+        fields.flatMap(x => x.headline).getOrElse("")
+      }.head.toString(),
+      issueDate = response.map { f =>
+        f.flatMap(x => x.newspaperEditionDate).get
+      }.head,
+      releaseDate = response.map { f =>
+        f.flatMap(x => x.newspaperEditionDate).get
+      }.head,
+      pubDate = response.map { f =>
+        f.flatMap(x => x.newspaperEditionDate).get
+      }.head,
+      byline = response.map { f =>
+        f.flatMap(x => x.byline).getOrElse("")
+      }.head.toString(),
+      articleAbstract = response.map { f =>
+        f.flatMap(x => x.standfirst).getOrElse("")
+      }.head.toString(),
+      content = response.map { f =>
+        f.flatMap(x => x.body).getOrElse("") // TODO: Should this x.body (with html tags) or x.bodyText?
+      }.head.toString()
+    )
+  }
+}
+
