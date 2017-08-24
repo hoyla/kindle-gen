4d1e2e5ce207c191c8c6e67c6b78ee1a94a02df2
Alice Dee <alice.dee@guardian.co.uk>
2017-08-24 21:57:15 +0100

    Extracts toNitf method into its own case class
    Tidys up Article case class by removing the toNitf method into a
    seperate ArticleNITF case class.

--- src/main/scala/com/gu/kindlegen/Article.scala
+++ src/main/scala/com/gu/kindlegen/Article.scala
@@ -8,55 +8,19 @@ import org.joda.time.DateTime
 import org.joda.time.format.DateTimeFormat
 
 case class Article(
-    newspaperBookSection: String,
-    sectionName: String,
-    newspaperPageNumber: Int,
-    title: String,
-    docId: String,
-    issueDate: CapiDateTime,
-    // newspaperEditionDate in ContentFields
-    releaseDate: CapiDateTime,
-    pubDate: CapiDateTime,
-    byline: String,
-    articleAbstract: String, // standfirst is used
-    content: String
-) {
-  def toNitf: String = {
-    val formattedIssueDate = formatter.print(isoFormatter.parseDateTime(capiIsoDateTimeToString(issueDate)))
-    val formattedReleaseDate = formatter.print(isoFormatter.parseDateTime(capiIsoDateTimeToString(releaseDate)))
-    val formattedPubDate = formatter.print(isoFormatter.parseDateTime(capiIsoDateTimeToString(pubDate)))
-    s"""
-       |<?xml version="1.0" encoding="UTF-8"?>
-       |<nitf version="-//IPTC//DTD NITF 3.3//EN">
-       |<head>
-       |<title>$title</title>
-       |<docdata management-status="usable">
-       |<doc-id id-string="$docId" />
-       |<urgency ed-urg="2" />
-       |<date.issue norm="$formattedIssueDate" />
-       |<date.release norm="$formattedReleaseDate" />
-       |<doc.copyright holder="guardian.co.uk" />
-       |</docdata>
-       |<pubdata type="print" date.publication="$formattedPubDate" />
-       |</head>
-       |<body>
-       |<body.head>
-       |<hedline><hl1>$title</hl1></hedline>
-       |<byline>$byline</byline>
-       |<abstract>$articleAbstract</abstract>
-       |</body.head>
-       |<body.content>$content</body.content>
-       |<body.end />
-       |</body>
-       |</nitf>""".stripMargin
-  }
-
-  // formatter for use with parseDateTime to convert an isoDateTime: String to isoDateTime: DateTime
-  private def isoFormatter = ISODateTimeFormat.dateTime()
-  // formatter for use with parseDateTime or print to convert a DateTime: Long to DateTime: DateTime
-  private def formatter = DateTimeFormat.forPattern("yyyyMMdd")
-  private def capiIsoDateTimeToString(dt: CapiDateTime): String = dt.iso8601
-}
+  newspaperBookSection: String,
+  sectionName: String,
+  newspaperPageNumber: Int,
+  title: String,
+  docId: String,
+  issueDate: CapiDateTime,
+  // newspaperEditionDate in ContentFields
+  releaseDate: CapiDateTime,
+  pubDate: CapiDateTime,
+  byline: String,
+  articleAbstract: String, // standfirst is used
+  content: String
+)
 
 object Article {
   def apply(content: Content) = new Article(
---
+++ src/main/scala/com/gu/kindlegen/ArticleNITF.scala
@@ -0,0 +1,43 @@
+package com.gu.kindlegen
+import com.gu.contentapi.client.model.v1.CapiDateTime
+import org.joda.time.format.ISODateTimeFormat
+import org.joda.time.format.DateTimeFormat
+
+/**
+ * Created by alice_dee on 24/08/2017.
+ */
+case class ArticleNITF(fileContents: String)
+
+object ArticleNITF {
+  def apply(article: Article) = new ArticleNITF(
+    fileContents = s"""
+     |<?xml version="1.0" encoding="UTF-8"?>
+     |<nitf version="-//IPTC//DTD NITF 3.3//EN">
+     |<head>
+     |<title>${article.title}</title>
+     |<docdata management-status="usable">
+     |<doc-id id-string="${article.docId}" />
+     |<urgency ed-urg="2" />
+     |<date.issue norm="${formatter.print(isoFormatter.parseDateTime(capiIsoDateTimeToString(article.issueDate)))}" />
+     |<date.release norm="${formatter.print(isoFormatter.parseDateTime(capiIsoDateTimeToString(article.releaseDate)))}" />
+     |<doc.copyright holder="guardian.co.uk" />
+     |</docdata>
+     |<pubdata type="print" date.publication="${formatter.print(isoFormatter.parseDateTime(capiIsoDateTimeToString(article.releaseDate)))}" />
+     |</head>
+     |<body>
+     |<body.head>
+     |<hedline><hl1>${article.title}</hl1></hedline>
+     |<byline>${article.byline}</byline>
+     |<abstract>${article.articleAbstract}</abstract>
+     |</body.head>
+     |<body.content>${article.content}</body.content>
+     |<body.end />
+     |</body>
+     |</nitf>""".stripMargin
+  )
+  // formatter for use with parseDateTime to convert an isoDateTime: String to isoDateTime: DateTime
+  private def isoFormatter = ISODateTimeFormat.dateTime()
+  // formatter for use with parseDateTime or print to convert a DateTime: Long to DateTime: DateTime
+  private def formatter = DateTimeFormat.forPattern("yyyyMMdd")
+  private def capiIsoDateTimeToString(dt: CapiDateTime): String = dt.iso8601
+}
