b7368fbd463647e6f3de0ac296db70e1db674de1
Alice Dee <alice.dee@guardian.co.uk>
2017-08-27 10:07:51 +0100

    Refactors test to use FlatSpec not JUnit in QuerierSpec

--- src/test/scala/com/gu/kindlegen/QuerierSpec.scala
+++ src/test/scala/com/gu/kindlegen/QuerierSpec.scala
@@ -1,40 +1,24 @@
 package com.gu.kindlegen
 
-import org.scalatest.FunSuite
-import org.junit.runner.RunWith
-import org.scalatest.junit.JUnitRunner
 import com.gu.contentapi.client.utils.CapiModelEnrichment
-import com.amazonaws.services.lambda.runtime.Context
-import com.gu.contentapi.client.model.v1.Content
 import org.joda.time.DateTime
 import org.joda.time.format.DateTimeFormat
-import scala.io.Source
 
-@RunWith(classOf[JUnitRunner])
-class QuerierSpec extends FunSuite {
+import org.scalatest.FlatSpec
+
+class QuerierSpec extends FlatSpec {
 
   // TODO: Find a way to override the source file to a sample.conf version
-  test("Querier.readApiKey reads key from external file") {
-    //    override val configSource = Source.fromFile("sample.conf")
+  ".readApiKey" should "read API key from external conf file" in {
     assert(Querier.readApiKey !== "test")
   }
 
-  // TODO: Find a way to test printSentResponse, extract the edition dates etc
-  // TODO: Find a way to test resultToArticle
-
-  test("Querier.toManifest converts Article to Manifest contents page") {
+  ".toManifest" should "convert article to manifest contents page" in {
+    val issueDate = CapiModelEnrichment.RichJodaDateTime(formatter.parseDateTime("20170724")).toCapiDateTime
     val article = Article(
       newspaperBookSection = "theguardian/mainsection/international",
       sectionName = "International",
-      newspaperPageNumber = 0,
-      title = "my title",
-      docId = "section/date/title",
-      issueDate = CapiModelEnrichment.RichJodaDateTime(formatter.parseDateTime("20170724")).toCapiDateTime,
-      releaseDate = CapiModelEnrichment.RichJodaDateTime(formatter.parseDateTime("20170724")).toCapiDateTime,
-      pubDate = CapiModelEnrichment.RichJodaDateTime(formatter.parseDateTime("20170724")).toCapiDateTime,
-      byline = "my name",
-      articleAbstract = "article abstract",
-      content = "content"
+      0, "my title", "", issueDate, issueDate, issueDate, "my name", "article abstract", "content"
     )
     val articles = List(article)
     val time = DateTime.now()
@@ -45,6 +29,9 @@ class QuerierSpec extends FunSuite {
       sections = List(SectionHeading("International", "theguardian/mainsection/international.xml"))
     ))
   }
+
+  // TODO: Find a way to test printSentResponse, extract the edition dates etc
+  // TODO: Find a way to test resultToArticle
 
   private def formatter = DateTimeFormat.forPattern("yyyyMMdd")
 
