89e7c1baf3a1cbcb22c126aaae8eee954e1dc137
Alice Dee <alice.dee@guardian.co.uk>
2017-08-27 08:50:51 +0100

    Refactor BookSectionToList method and move to BookSection class

--- src/main/scala/com/gu/kindlegen/BookSection.scala
+++ src/main/scala/com/gu/kindlegen/BookSection.scala
@@ -9,3 +9,20 @@ case class BookSection(bookSectionId: String, bookSectionTitle: String, pages: L
   def numPages = pages.size
   def numArticles = pages.flatMap(_.articles).size
 }
+
+object BookSection {
+
+  def chunkBookSectionPages(pages: List[BookSectionPage]): List[List[BookSectionPage]] = {
+    val tsil = pages.foldLeft(List.empty[List[BookSectionPage]]) {
+      // case where the id of the first page in the first list is the same as current enum page id.
+      case ((head :: tail), page) if head.map(_.bookSectionId).head == page.bookSectionId => {
+        (page :: head) :: tail
+      }
+      // case where ids do not match (ie the same pattern as above but not caught by the above case if statement) This pattern will also catch the empty initial list
+      case (lst, page) =>
+        List(page) :: lst
+    }
+    tsil.reverse
+  }
+
+}
---
+++ src/test/scala/com/gu/kindlegen/BookSectionSuite.scala
@@ -0,0 +1,50 @@
+package com.gu.kindlegen
+
+import org.scalatest.FunSuite
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import com.github.nscala_time.time.Imports._
+import com.gu.contentapi.client.model.v1.Content
+import com.gu.contentapi.client.utils._
+
+@RunWith(classOf[JUnitRunner])
+class BookSectionSuite extends FunSuite {
+
+  private def formatter = DateTimeFormat.forPattern("yyyyMMdd")
+
+  val ta = Article(TestContent("", "", 1, "", "", CapiModelEnrichment.RichJodaDateTime(formatter.parseDateTime("20170724")).toCapiDateTime, "", "", "").toContent)
+  val articles = {
+    List(
+      ("theguardian/mainsection/international", 1),
+      ("theguardian/mainsection/international", 2),
+      ("theguardian/mainsection/international", 2),
+      ("theguardian/mainsection/international", 3),
+      ("theguardian/mainsection/topstories", 4),
+      ("theguardian/mainsection/topstories", 4)
+    ).map {
+
+        case (m, n) => ta.copy(newspaperBookSection = m, newspaperPageNumber = n)
+      }
+  }
+
+  val chunkedArticles = BookSectionPage.chunkByPageNum(articles)
+  println(s"articles = ${articles}")
+  println(s"chunked articles = ${chunkedArticles}")
+  val BSPs = BookSectionPage.chunksToBSP(chunkedArticles)
+
+  test("chunkBookSectionPagesByBookSection") {
+    assert(BookSection.chunkBookSectionPages(BSPs).map(_.map(_.bookSectionId)) ===
+      List(
+        List(
+          "theguardian/mainsection/international",
+          "theguardian/mainsection/international",
+          "theguardian/mainsection/international"
+        ),
+        List(
+          "theguardian/mainsection/topstories"
+        )
+      ))
+  }
+  //      s"BookSection ${bs.bookSectionId} contains pages ${bs.}
+
+}
