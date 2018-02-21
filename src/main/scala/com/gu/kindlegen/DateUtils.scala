1c51c74d7db999dc07e6914619a9ce7a4529b2a4
Hosam Aly <hosam.aly.freelance@guardian.co.uk>
2018-02-21 12:07:09 +0000

    Simplify DateUtils and remove the implicit conversion to Instant

--- src/main/scala/com/gu/kindlegen/DateUtils.scala
+++ src/main/scala/com/gu/kindlegen/DateUtils.scala
@@ -1,10 +1,11 @@
 package com.gu.kindlegen
 
 import java.time.format.DateTimeFormatter
-import java.time.temporal.{ChronoField, TemporalAccessor}
-import java.time.{Instant, LocalDate, ZoneId, ZonedDateTime}
+import java.time.temporal.{ChronoUnit, TemporalAccessor}
+import java.time.{Instant, LocalDate, ZoneId}
 
 import com.gu.contentapi.client.model.v1.CapiDateTime
+import com.gu.contentapi.client.utils.CapiModelEnrichment.RichCapiDateTime
 
 object DateUtils {
   // formatter for use with print to convert a DateTime: Long to DateTime String
@@ -15,49 +16,28 @@ object DateUtils {
   private def isoFormatter = DateTimeFormat.isoDateTime()
   private def formatterWithDashes = DateTimeFormat.forPattern("yyyy-MM-dd")
 
-  def formatDate(capiDate: CapiDateTime): String = formatter.format(isoFormatter.parseDate(capiDate.iso8601))
+  def formatDate(capiDate: CapiDateTime): String = formatter.format(capiDate.toOffsetDateTime)
 
   // TODO: change this to DateTime.now or function that takes a passed in date.
-  // TODO: should be Z format not +XXXX hours?
-  private def editionDateTime: DateTime = formatterWithDashes.parseDate("2017-05-19") // to have a date I know the results for
+  private def editionDateTime: Instant = Instant.parse("2017-05-19T00:00:00Z") // to have a date I know the results for
   //  def editionDateTime: DateTime = DateTime.now()
 
-  def editionDateStart: DateTime = editionDateTime.startOfDay
-  def editionDateEnd: DateTime = editionDateTime.endOfDay
+  def editionDateStart: Instant = editionDateTime
+  def editionDateEnd: Instant = editionDateTime.plus(1, ChronoUnit.DAYS).minus(1, ChronoUnit.NANOS)
 
   /* Adaptor classes to simplify the migration from Joda-Time to Java 8 Time */
 
-  protected[DateUtils] trait ReadableInstant {
-    def toInstant: Instant
-    def toCapiDateTime: CapiDateTime = {
-      val dt = toInstant
-      CapiDateTime.apply(dt.toEpochMilli, dt.toString)
-    }
-  }
-
-  import scala.language.implicitConversions
-  implicit def readableInstantAdaptor2ReadableInstant(readableInstantAdaptor: ReadableInstant): Instant =
-    readableInstantAdaptor.toInstant
-
   object DateTime {
-    def parse(pattern: String) =
-      new DateTime(DateTimeFormatter.ISO_DATE_TIME.parse(pattern))
     def now() =
       new DateTime(Instant.now)
   }
 
-  final class DateTime(protected[DateUtils] val instant: Instant) extends ReadableInstant {
+  final class DateTime(instant: Instant) {
     def this(temporalAccessor: TemporalAccessor) =
       this(Instant.from(temporalAccessor))
 
-    override def toInstant: Instant = instant
-
-    import ChronoField._
-    def startOfDay: DateTime = withField(MILLI_OF_DAY, MILLI_OF_DAY.range.getMinimum)
-    def endOfDay: DateTime = withField(MILLI_OF_DAY, MILLI_OF_DAY.range.getMaximum)
-
-    private def withField(field: ChronoField, value: Long): DateTime =
-      new DateTime(instant.`with`(field, value))
+    def toCapiDateTime: CapiDateTime =
+      CapiDateTime.apply(instant.toEpochMilli, instant.toString)
   }
 
   object DateTimeFormat {
@@ -75,6 +55,8 @@ object DateUtils {
     }
 
     def format(dateTime: DateTime): String =
-      formatter.format(ZonedDateTime.ofInstant(dateTime.instant, ZoneId.systemDefault()))
+      formatter.format(dateTime.toCapiDateTime.toOffsetDateTime)
+
+    def format(temporal: TemporalAccessor) = formatter.format(temporal)
   }
 }
