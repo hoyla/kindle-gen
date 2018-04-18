c631db6c85dbe03bababd42e9478611f83c96c14
Hosam Aly <hosam.aly.freelance@guardian.co.uk>
2018-04-18 11:46:00 +0100

    Download the highest resolution (master) images
    Amazon will scale them down as needed.

--- src/main/scala/com/gu/kindlegen/Article.scala
+++ src/main/scala/com/gu/kindlegen/Article.scala
@@ -1,10 +1,8 @@
 package com.gu.kindlegen
 
-import com.gu.contentapi.client.model.v1.Asset
-import com.gu.contentapi.client.model.v1.Element
+import com.gu.contentapi.client.model.v1.{Asset, Element, _}
 import com.gu.contentapi.client.model.v1.ElementType.Image
 import com.gu.contentapi.client.model.v1.TagType.NewspaperBookSection
-import com.gu.contentapi.client.model.v1._
 
 case class Article(
     newspaperBookSection: String,
@@ -31,12 +29,13 @@ object Article {
   def getImageUrl(content: Content): Option[String] = {
     def isMainImage(e: Element): Boolean =
       e.`type` == Image && e.relation == "main"
-    def isCorrectSizedAsset(a: Asset): Boolean =
-      a.typeData.flatMap(_.width) == Some(500)
 
-    val elements = content.elements
-    val mainImage = elements.flatMap(_.find(isMainImage))
-    val imageAsset = mainImage.flatMap(_.assets.find(isCorrectSizedAsset))
+    def isMasterImage(a: Asset): Boolean =
+      a.typeData.flatMap(_.isMaster).getOrElse(false)
+
+    val elements = content.elements.getOrElse(Nil)
+    val mainImage = elements.find(isMainImage)
+    val imageAsset = mainImage.flatMap(_.assets.find(isMasterImage))
     // keep imageUrl as an option otherwise later you will try to download form an empty string
     val imageUrl = imageAsset.flatMap(_.typeData).flatMap(_.secureFile)
     imageUrl
