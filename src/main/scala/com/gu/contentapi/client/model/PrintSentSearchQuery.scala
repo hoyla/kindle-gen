44005e840676033039d998b06a590a747c11f04e
Hosam Aly <hosam.aly.freelance@guardian.co.uk>
2018-04-18 18:56:26 +0100

    Fix StackOverflowException introduced by the previous commit

--- src/main/scala/com/gu/contentapi/client/model/PrintSentSearchQuery.scala
+++ src/main/scala/com/gu/contentapi/client/model/PrintSentSearchQuery.scala
@@ -4,7 +4,7 @@ import com.gu.contentapi.client.{Parameter, Parameters}
 
 object PrintSentSearchQuery {
   def apply(parameterHolder: Map[String, Parameter] = Map.empty): PrintSentSearchQuery = {
-    new PrintSentSearchQuery(parameterHolder).tier("internal")
+    new PrintSentSearchQuery(Map.empty).withParameters(parameterHolder)
   }
 }
 
@@ -13,7 +13,12 @@ final case class PrintSentSearchQuery private(parameterHolder: Map[String, Param
      with TierParameter[PrintSentSearchQuery] {
 
   override def pathSegment: String = "content/print-sent"
-  override def withParameters(parameterMap: Map[String, Parameter]) = PrintSentSearchQuery(parameterMap)
+
+  override def withParameters(parameterMap: Map[String, Parameter]) = new PrintSentSearchQuery(
+    if (parameterMap.contains(tier.name))
+      parameterMap
+    else
+      parameterMap.updated(tier.name, tier.withValue(Some("internal"))))
 }
 
 trait TierParameter[Owner <: Parameters[Owner]] extends Parameters[Owner] { this: Owner =>
