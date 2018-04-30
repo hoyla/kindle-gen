05389ff9b0288afdcd91f527ff60c2dce03844a1
Hosam Aly <hosam.aly.freelance@guardian.co.uk>
2018-04-30 16:24:20 +0100

    Remove (the unused) Section.ordering

--- src/main/scala/com/gu/kindlegen/Section.scala
+++ src/main/scala/com/gu/kindlegen/Section.scala
@@ -8,6 +8,4 @@ case class Section(id: String, title: String, link: Link)
 
 object Section {
   def apply(tag: Tag): Section = Section(tag.id, tag.webTitle, AbsoluteURL.from(tag.webUrl))
-
-  implicit val ordering: Ordering[Section] = Ordering.by(section => (section.id, section.title))
 }
