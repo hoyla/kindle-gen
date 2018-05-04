991322527d19e8667eb8b621d0daf19f4bdbf0bf
Hosam Aly <hosam.aly.freelance@guardian.co.uk>
2018-05-04 18:32:32 +0100

    Test the isolation of /tmp

--- src/main/scala/com/gu/kindlegen/Lambda.scala
+++ src/main/scala/com/gu/kindlegen/Lambda.scala
@@ -1,43 +1,43 @@
 package com.gu.kindlegen
 
+import java.nio.file.{Files, Paths}
+import java.time.{Instant, LocalDate, ZoneOffset}
+
 import com.amazonaws.services.lambda.runtime.Context
-/**
- * This is compatible with aws' lambda JSON to POJO conversion.
- * You can test your lambda by sending it the following payload:
- * {"name": "Bob"}
- */
-class LambdaInput() {
-  var name: String = _
-  def getName(): String = name
-  def setName(theName: String): Unit = name = theName
-}
-
-case class Env(app: String, stack: String, stage: String) {
-  override def toString: String = s"App: $app, Stack: $stack, Stage: $stage\n"
-}
-
-object Env {
-  def apply(): Env = Env(
-    Option(System.getenv("App")).getOrElse("DEV"),
-    Option(System.getenv("Stack")).getOrElse("DEV"),
-    Option(System.getenv("Stage")).getOrElse("DEV")
-  )
-}
+import scala.collection.JavaConverters._
+import scala.util.{Failure, Success}
 
 object Lambda {
+  // env.AWS_REGION -> eu-west-1
+  // env.Stage -> CODE
 
   /*
    * This is your lambda entry point
    */
-  def handler(lambdaInput: LambdaInput, context: Context): Unit = {
+  def handler(parameters: java.util.Map[String, String], context: Context): Unit = {
     val logger = context.getLogger
-    val env = Env()
-    logger.log(s"Starting $env")
-    logger.log(process(lambdaInput.name, env))
+
+    val params = parameters.asScala
+    val date =
+      params.get("date").map(LocalDate.parse)  // custom parameter in the test view
+        .orElse(params.get("time").map(Instant.parse(_).atZone(ZoneOffset.UTC).toLocalDate))  // scheduled event
+        .getOrElse(LocalDate.now)
+
+    val tmpFiles = Files.list(Paths.get("/tmp")).iterator.asScala.toList
+    println(s"Found ${tmpFiles.length} tmp files:\n${tmpFiles.mkString(", ")}")
+
+    val newFile = Files.createFile(Paths.get(s"/tmp/$date"))
+    println(s"Created a new temp file: $newFile")
+
+//    logger.log(s"Generating for $date")
+//
+//    Settings.load match {
+//      case Success(settings) =>
+//        val kindleGenerator = KindleGenerator(settings, date)
+//        kindleGenerator.writeNitfBundleToDisk()
+//        // TODO write the results to S3
+//      case Failure(error) => logger.log(s"[ERROR] Could not load the configuration! $error")
+//    }
   }
 
-  /*
-   * I recommend to put your logic outside of the handler
-   */
-  def process(name: String, env: Env): String = s"Hello $name! (from ${env.app} in ${env.stack})\n"
 }
