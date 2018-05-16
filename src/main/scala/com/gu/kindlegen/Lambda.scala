36c9060ea72f86c45805b1e4a975814225ff80b5
Hosam Aly <hosam.aly.freelance@guardian.co.uk>
2018-05-16 18:46:27 +0100

    Report errors that result from KindleGenerator#publish

--- src/main/scala/com/gu/kindlegen/Lambda.scala
+++ src/main/scala/com/gu/kindlegen/Lambda.scala
@@ -4,21 +4,27 @@ import java.time.{Instant, LocalDate}
 import java.time.ZoneOffset.UTC
 
 import scala.collection.JavaConverters._
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{Await, ExecutionContext}
+import scala.concurrent.duration._
 import scala.util.{Failure, Success}
 
-import com.amazonaws.services.lambda.runtime.Context
+import com.amazonaws.services.lambda.runtime.{Context, LambdaLogger}
 import com.amazonaws.services.s3.AmazonS3ClientBuilder
 import com.typesafe.config.{Config, ConfigFactory}
 
 import com.gu.io.aws.S3Publisher
 
 object Lambda {
+  private val ErrorReportingTimeInMillis = 10
+
   /*
    * This is your lambda entry point
    */
   def handler(parameters: java.util.Map[String, Any], context: Context): Unit = {
-    val logger = context.getLogger
+    def reportError(msg: String): PartialFunction[Throwable, Settings] = { case error =>
+      context.getLogger.log(s"[ERROR] $msg! $error")
+      throw error
+    }
 
     val params = parameters.asScala.mapValues(_.toString)
 
@@ -29,20 +35,23 @@ object Lambda {
 
     val config = resolveConfig(sys.env.getOrElse("ConfigSettings", ""))
 
-    Settings(config).map(withOutputDirForDate(date, _)) match {
-      case Success(settings) =>
-        import scala.concurrent.ExecutionContext.Implicits.global
+    Settings(config)
+      .recover(reportError("Could not load the configuration"))
+      .map(withOutputDirForDate(date))
+      .map(run(date, context))
+      .recover(reportError("Generation failed!"))
+  }
 
-        val publisher = s3Publisher(settings.s3)
-        val kindleGenerator = KindleGenerator(settings, date, publisher)
+  private def run(date: LocalDate, context: Context)(settings: Settings) = {
+    import scala.concurrent.ExecutionContext.Implicits.global
 
-        logger.log(s"Generating for $date; uploading to s3://${settings.s3.absolutePath.source}")
-        kindleGenerator.publish()
+    val publisher = s3Publisher(settings.s3)
+    val kindleGenerator = KindleGenerator(settings, date, publisher)
 
-      case Failure(error) =>
-        logger.log(s"[ERROR] Could not load the configuration! $error")
-        throw error
-    }
+    context.getLogger.log(s"Generating for $date; uploading to s3://${settings.s3.absolutePath.source}")
+    val published = kindleGenerator.publish()
+
+    Await.ready(published, Duration(context.getRemainingTimeInMillis - ErrorReportingTimeInMillis, MILLISECONDS))
   }
 
   private def resolveConfig(configString: String): Config = {
@@ -51,7 +60,7 @@ object Lambda {
     overrides.withFallback(config).resolve()
   }
 
-  private def withOutputDirForDate(date: LocalDate, settings: Settings) = {
+  private def withOutputDirForDate(date: LocalDate)(settings: Settings) = {
     val fileSettings = settings.publishing.files
     val originalOutputDir = fileSettings.outputDir.toAbsolutePath
     val customFileSettings = fileSettings.copy(outputDir = originalOutputDir.resolve(date.toString))
