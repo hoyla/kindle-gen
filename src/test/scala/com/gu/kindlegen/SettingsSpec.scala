7e1adebbf8edf8d63ce30a4b5f2bc424786c3e2c
Hosam Aly <hosam.aly.freelance@guardian.co.uk>
2018-06-12 15:34:07 +0100

    Refactor SettingsSpec for more comprehensive testing
    And test that we're able to read the configuration file.

--- src/test/scala/com/gu/kindlegen/SettingsSpec.scala
+++ src/test/scala/com/gu/kindlegen/SettingsSpec.scala
@@ -4,24 +4,58 @@ import java.nio.file.Paths
 import java.time.Duration
 
 import scala.collection.JavaConverters._
+import scala.util.{Failure, Success, Try}
 
 import com.typesafe.config._
-import org.scalatest.FunSpec
+import org.scalatest.{Assertion, FunSpec}
 import org.scalatest.Matchers._
 
+import com.gu.config.ConfigReader
 import com.gu.contentapi.client.model.v1.TagType
 import com.gu.scalatest.PathMatchers._
 
 
-object SettingsSpec {
-  implicit class RichMap(val values: Map[String, _]) extends AnyVal {
-    def toConfig: Config = toConfigObj.toConfig
-    def toConfigObj: ConfigObject = ConfigValueFactory.fromMap(values.asJava)
+class SettingsSpec extends FunSpec {
+  import SettingsSpec._
+
+  testReader(ContentApiSettings, contentApiConfig)(validateValues)
+
+  testReader(PublishedFileSettings, publishedFilesConfig)(validateValues)
+
+  testReader(PublishingSettings, publishingConfig)(validateValues)
+
+  testReader(GuardianProviderSettings, capiConfig)(validateValues)
+
+  testReader(S3Settings, s3Config)(validateValues)
+
+  testReader(Settings, settingsConfig)(validateValues)
+
+  describe("Settings.load") {
+    it("reads the configuration file correctly") {
+      validateSettings(Settings.load) { _: Settings => /* just test that it is read without errors */ }
+    }
+  }
+
+  private def testReader[T](reader: ConfigReader[T], sampleConfig: ConfigObject)(validator: T => Assertion) = {
+    describe(reader.getClass.getSimpleName) {
+      it("reads a sample configuration correctly") {
+        validateSettings(reader(sampleConfig.toConfig))(validator)
+      }
+    }
+  }
+
+  private def validateSettings[T](maybeSettings: Try[T])(validate: T => _) = {
+    maybeSettings match {
+      case Success(settings) => validate(settings)
+      case Failure(error) => fail("Failed to read configuration", error)
+    }
   }
 }
 
-class SettingsSpec extends FunSpec {
-  import SettingsSpec._
+object SettingsSpec {
+  implicit class RichMap(val values: Map[String, _]) extends AnyVal {
+    def toConfigObj: ConfigObject = ConfigValueFactory.fromMap(values.asJava)
+  }
 
   private val contentApiValues = Map(
     "key" -> "My API key",
@@ -40,7 +74,7 @@ class SettingsSpec extends FunSpec {
     "publicationName" -> "My Publication",
     "publicationLink" -> "http://example.com",
     "prettifyXml" -> "on",
-    "files" -> publishedFilesValues.toConfigObj,
+    "files" -> publishedFilesConfig,
     "images" -> Map("download" -> "on").toConfigObj
   )
 
@@ -57,60 +91,58 @@ class SettingsSpec extends FunSpec {
     "tmpDirOnDisk" -> "/tmp"
   )
 
-  private def settingsConfig = Map(
+  private val settingsValues = Map(
     "content-api" -> contentApiConfig,
     "publishing" -> publishingConfig,
     "gu-capi" -> capiConfig,
     "s3" -> s3Config,
-  ).toConfig
+  )
+
+  private def settingsConfig = settingsValues.toConfigObj
   private def contentApiConfig = contentApiValues.toConfigObj
   private def publishingConfig = publishingValues.toConfigObj
+  private def publishedFilesConfig = publishedFilesValues.toConfigObj
   private def capiConfig = capiValues.toConfigObj
   private def s3Config = s3Values.toConfigObj
 
-  describe("Settings factory") {
-    val settings = Settings(settingsConfig).get
+  private def validateValues(settings: Settings): Assertion = {
+    validateValues(settings.contentApi)
+    validateValues(settings.provider)
+    validateValues(settings.publishing)
+    validateValues(settings.s3)
+  }
 
-    it("parses ContentApiSettings correctly") {
-      val contentApiSettings = settings.contentApi
+  private def validateValues(contentApiSettings: ContentApiSettings): Assertion = {
+    contentApiSettings.apiKey shouldBe contentApiValues("key")
+    contentApiSettings.targetUrl shouldBe contentApiValues("url")
+  }
 
-      contentApiSettings.apiKey shouldBe contentApiValues("key")
-      contentApiSettings.targetUrl shouldBe contentApiValues("url")
-    }
+  private def validateValues(publishingSettings: PublishingSettings): Assertion = {
+    publishingSettings.downloadImages shouldBe true
+    publishingSettings.prettifyXml shouldBe true
+    publishingSettings.minArticlesPerEdition shouldBe publishingValues("minArticlesPerEdition")
+    publishingSettings.publicationName shouldBe publishingValues("publicationName")
+    publishingSettings.publicationLink shouldBe publishingValues("publicationLink")
 
-    it("parses PublishingSettings correctly") {
-      val publishingSettings = settings.publishing
+    validateValues(publishingSettings.files)
+  }
 
-      publishingSettings.downloadImages shouldBe true
-      publishingSettings.prettifyXml shouldBe true
-      publishingSettings.minArticlesPerEdition shouldBe publishingValues("minArticlesPerEdition")
-      publishingSettings.publicationName shouldBe publishingValues("publicationName")
-      publishingSettings.publicationLink shouldBe publishingValues("publicationLink")
-    }
+  private def validateValues(fileSettings: PublishedFileSettings): Assertion = {
+    fileSettings.outputDir should beTheSameFileAs(Paths.get(publishedFilesValues("outputDir")))
+    fileSettings.nitfExtension shouldBe publishedFilesValues("nitfExtension")
+    fileSettings.rssExtension shouldBe publishedFilesValues("rssExtension")
+    fileSettings.rootManifestFileName shouldBe publishedFilesValues("rootManifestFileName")
+  }
 
-    it("parses PublishedFileSettings correctly") {
-      val fileSettings = settings.publishing.files
+  private def validateValues(providerSettings: GuardianProviderSettings): Assertion = {
+    Duration.ofNanos(providerSettings.downloadTimeout.toNanos) shouldBe capiValues("downloadTimeout")
+    providerSettings.maxImageResolution shouldBe capiValues("maxImageResolution")
+    providerSettings.sectionTagType shouldBe sectionTagType
+  }
 
-      fileSettings.outputDir should beTheSameFileAs(Paths.get(publishedFilesValues("outputDir")))
-      fileSettings.nitfExtension shouldBe publishedFilesValues("nitfExtension")
-      fileSettings.rssExtension shouldBe publishedFilesValues("rssExtension")
-      fileSettings.rootManifestFileName shouldBe publishedFilesValues("rootManifestFileName")
-    }
-
-    it("parses GuardianProviderSettings correctly") {
-      val providerSettings = settings.provider
-
-      Duration.ofNanos(providerSettings.downloadTimeout.toNanos) shouldBe capiValues("downloadTimeout")
-      providerSettings.maxImageResolution shouldBe capiValues("maxImageResolution")
-      providerSettings.sectionTagType shouldBe sectionTagType
-    }
-
-    it("parses S3Settings correctly") {
-      val s3Settings = settings.s3
-
-      s3Settings.bucketName shouldBe s3Values("bucket")
-      s3Settings.bucketDirectory shouldBe s3Values("prefix")
-      s3Settings.tmpDirOnDisk.toString shouldBe s3Values("tmpDirOnDisk")
-    }
+  private def validateValues(s3Settings: S3Settings): Assertion = {
+    s3Settings.bucketName shouldBe s3Values("bucket")
+    s3Settings.bucketDirectory shouldBe s3Values("prefix")
+    s3Settings.tmpDirOnDisk.toString shouldBe s3Values("tmpDirOnDisk")
   }
 }
